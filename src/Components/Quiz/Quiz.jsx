1d53ad2cf05132825497514ece94d9a4cf73d12c
Md. Ashraful <112319085+ashraful5158@users.noreply.github.com>
2024-02-21 12:07:43 +0600

    done

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -3,7 +3,7 @@ import "./Quiz.css";
 import { data } from "../../assets/data";
 
 const Quiz = () => {
-  let [index, setIndex] = useState(1);
+  let [index, setIndex] = useState(0);
   let [question, setQuestion] = useState(data[index]);
   let [lock, setLock] = useState(false);
   let [score, setScore] = useState(0);
